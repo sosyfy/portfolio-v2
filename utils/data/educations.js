309e1972b57cb602e7a395a4c58243ceb3a62225
sosyfy <sospeterkaranja02@gmail.com>
2024-05-06 16:31:07 +0300

    Initial build

---
+++ utils/data/educations.js
@@ -0,0 +1,14 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor of Science in Software Engineering",
+    duration: "2020 - 2024",
+    institution: "Multimedia University of Kenya",
+  },
+  {
+    id: 2,
+    title: "Cool Certificate in Web Development",
+    duration: "2020- forever",
+    institution: "The internet School",
+  }
+]
---
+++ utils/data/experience.js
@@ -0,0 +1,20 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Software Engineer I',
+    company: "Tenn Technoologies Ltd.",
+    duration: "(July 2023 - Present)"
+  },
+  {
+    id: 3,
+    title: "Software Engineer Intern",
+    company: "Nouveta Technologies Ltd.",
+    duration: "(April 2022 - May 2024)"
+  },
+  {
+    id: 2,
+    title: "FullStack Developer",
+    company: "Fiverr (freelance)",
+    duration: "(Jun 2021 - Present)"
+  },
+]
