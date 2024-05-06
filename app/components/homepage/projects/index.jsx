545fe6efe5829ca5e488779254483e9f8922f2c9
sosyfy <sospeterkaranja02@gmail.com>
2024-05-06 17:45:35 +0300

    new modifications

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -1,21 +1,40 @@
-import { projectsData } from '@/utils/data/projects-data';
-import ProjectCard from './project-card';
+import { projectsData } from '@/utils/data/projects-data'
+import ProjectCard from './project-card'
 
 const Projects = () => {
-
   return (
-    <div id='projects' className="relative z-50  my-12 lg:my-24">
+    <div
+      id="projects"
+      className="relative z-50  my-12 lg:my-32"
+    >
       <div className="sticky top-10">
-        <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl  opacity-30"></div>
-        <div className="flex items-center justify-start relative">
-          <span className="bg-[#1a1443] absolute left-0  w-fit text-white px-5 py-3 text-xl rounded-md">
+        {/* <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl  opacity-30"></div>
+        <div className="flex items-center justify-center text-center relative">
+          <span className="bg-[#1a1443] absolute left-1/2 w-fit top-4 text-white px-5 py-3 text-xl rounded-md">
             PROJECTS
           </span>
           <span className="w-full h-[2px] bg-[#1a1443]"></span>
+        </div> */}
+
+        <div className="w-[100px] h-[100px] bg-violet-100 rounded-full absolute top-6 left-[42%] translate-x-1/2 filter blur-3xl  opacity-20"></div>
+        <div className="flex justify-center -translate-y-[1px]">
+          <div className="w-full">
+            <div className="h-[2px] bg-gradient-to-r from-transparent via-violet-500 to-transparent  w-full" />
+          </div>
+        </div>
+
+        <div className="flex justify-center my-5 lg:py-8">
+          <div className="flex  items-center">
+            <span className="w-24 h-[2px] bg-[#1a1443]"></span>
+            <span className="bg-[#1a1443] w-fit uppercase text-white p-2 px-5 text-xl rounded-md">
+              Projects
+            </span>
+            <span className="w-24 h-[2px] bg-[#1a1443]"></span>
+          </div>
         </div>
       </div>
 
-      <div className="pt-24">
+      <div className="pt-40">
         <div className="flex flex-col gap-6">
           {projectsData.slice(0, 4).map((project, index) => (
             <div
@@ -31,7 +50,7 @@ const Projects = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Projects;
+export default Projects
--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import { GoogleTagManager } from '@next/third-parties/google';
+// import { GoogleTagManager } from '@next/third-parties/google';
 import { Inter } from 'next/font/google';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -10,7 +10,8 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Portfolio of Sospeter Karanja - Software Developer',
-  description: 'This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.',
+  
+  description: 'This is the portfolio of Sospeter Karanja. I am an experienced full stack developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.',
 };
 
 export default function RootLayout({ children }) {
@@ -24,7 +25,7 @@ export default function RootLayout({ children }) {
         </main>
         <Footer />
       </body>
-      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
+      {/* <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} /> */}
     </html>
   )
 };
